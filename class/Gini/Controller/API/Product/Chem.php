15b6cb9b18e2bea2e42d702b384c1c606346c9f7
Jinlin Li <jinlin.li@geneegroup.com>
2016-01-13 14:34:34 +0800

    chem-db add 2 api search & get

--- class/Gini/Controller/API/Product/Chem.php
+++ class/Gini/Controller/API/Product/Chem.php
@@ -10,10 +10,51 @@ namespace Gini\Controller\API\Product;
 /**
  * @brief 继承自Gini\Controller\API\Base
  */
-class Chem extends \Gini\Controller\API\Base
+class Chem extends \Gini\Controller\API
 {
-    public function actionGetWastes($token, $start=0, $perpage = 25)
+    public function actionSearchProducts(array $criteria)
     {
+		$db = \Gini\Database::db();
+		$params = [];
+		$types = ['highly_toxic', 'drug_precursor', 'hazardous'];
+		$sql = "SELECT * FROM product ";
+		if (isset($criteria['type']) && in_array($criteria['type'], $types)) {
+			$sql .= "WHERE type=:type";
+			$params['type'] = $type;
+		}
+		$products = $db->query($sql, null, $params)->rows();
+		$count = count($products);
+		$token = md5(J($criteria));
+		$_SESSION[$token] = [
+			'criteria' => $criteria,
+			'sql' => $sql,
+			'params' => $params
+		];
+		return [
+			'token' => $token,
+			'count' => $count,
+		];
+    }
+
+    public function actionGetProducts($token, $start = 0, $perpage = 25)
+    {
+		$start = is_numeric($start) ? $start : 0;
+		$perpage = min($perpage, 25);
+		$db = \Gini\Database::db();
+		$params =  $_SESSION[$token];
+		$sql = $params['sql'].' LIMIT '.$start.','.$perpage;
+		$params = $params['params'];
+		$products = $db->query($sql, null, $params)->rows();
+		$data = [];
+		foreach ($products as $product) {
+			$data[$product->id] = [
+				'cas_no' => $product->cas_no,
+				'name' => $product->name,
+				'type' => $product->type,
+
+			];
+		}
+		return $data;
     }
 }
 ?>
