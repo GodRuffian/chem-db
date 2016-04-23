a6534b456e2f7924575086125c492366944ced41
Jia Huang <iamfat@gmail.com>
2016-04-23 17:50:44 +0800

    leejinlin 担心cas_no可能以后会取得长

--- class/Gini/ORM/Product.php
+++ class/Gini/ORM/Product.php
@@ -6,7 +6,7 @@ class Product extends Object
 {
     public $type = 'string:20'; // 化学试剂类别 易制毒、危险品、剧毒品、普通试剂
     public $name = 'string:150'; // 商品名称
-    public $cas_no = 'string:20'; // 化学试剂CAS号
+    public $cas_no = 'string:40'; // 化学试剂CAS号
     public $state = 'string:20';
 
     protected static $db_index = [
