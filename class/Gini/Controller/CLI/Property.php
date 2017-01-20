6c21940b55a4b121979c76832b40c3178e5651a9
Wenjun Zheng <wenjun.zheng@geneegroup.com>
2017-01-20 14:42:37 +0800

    试剂共存化学性质增加

---
+++ class/Gini/Controller/CLI/Property.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Gini\Controller\CLI;
+
+class Property extends \Gini\Controller\CLI
+{
+    public function actionFillData($params =[])
+    {
+        $csv = new \Gini\CSV(APP_PATH.'/'.DATA_DIR.'/'.$params[0], 'r');
+        $csv->read();
+        $line = 2;
+        while ($data = $csv->read()) {
+            if ($data['1']) {
+                $type = a('chemical/type');
+                $type->cas_no = $data['1'];
+                $type->name = $data['2'];
+
+                if (!$type->save()) {
+                    echo $line."--fail \n";
+                    $line++;
+                    continue ;
+                }
+                echo $line."--done \n";
+            }
+            $line++;
+        }
+        $csv->close();
+    }
+}
